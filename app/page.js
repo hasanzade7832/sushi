fb18aa21f1e0dfe773a009187cc75addd8932299
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2024-11-06 01:24:21 +0330

    added shopping cart ui

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <div>
         <Header />
         <SushiList />
-        {/* <DetailsProduct /> */}
+        <DetailsProduct />
       </div>
     </CartProvider>
   );
